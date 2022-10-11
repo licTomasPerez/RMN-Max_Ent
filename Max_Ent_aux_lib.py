45621ee11b61acc35ec9460ac2079750fbab1376
Tomás Pérez <54460449+licTomasPerez@users.noreply.github.com>
2022-10-11 19:40:08 -0300

    Update Max_Ent_aux_lib.py

--- Max_Ent_aux_lib.py
+++ Max_Ent_aux_lib.py
@@ -1,46 +1,46 @@
 # In [1]:
 
 import qutip
+import matplotlib.pyplot as plt 
 import numpy as np
-import scipy.optimize as opt
+import scipy.optimize as opt 
 import scipy.linalg as linalg
-import sys
 import math, cmath
 
-# In [2]:
+# In [1]:
+
+def A_rho_m_mplus2(m):
+    return np.e**(-m)
+
+def B_rho_m_mminus2(m):
+    return np.e**(-m)
+
+# In [3]:
 
 def generating_function_M_matrix(total_no_spins, cmt0_coeff_list, param_list, cbc = False):
-    
     cm_list = cmt0_coeff_list
     m_matrix_list = []
     M = total_no_spins
-    
     p = param_list[0]
     
-    ### if open boundary conditions the matrix, some m+2 and m-2 elements dissapear for some values of m. 
-    
-    if not cbc:  
-        for m in range(M):
-            if m == 0:
-                m_matrix_list.append(([-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)] + [0]
+    for m in range(M):
+        if m == 0:
+            m_matrix_list.append(([-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)] + [0]
                                     + [np.e**(-m+2)] + [0 for k in range(M-3)])) 
-            if m == 1:
-                local_list = [0] + [-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)] + [0] + [np.e**(-m+2)]; local_length = len(local_list)
-                m_matrix_list.append(local_list + [0 for k in range(M-local_length)])
-            if (m > 1) and (m < M - 2):  
-                local_list = [0 for j in range(m-2)]; local_length = len(local_list)
-                local_list += [np.e**(-m-2)] + [0] + [-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)] + [0] + [np.e**(-m+2)]
-                local_list += [0 for j in range(M-local_length - 5)]
-                m_matrix_list.append(local_list)
-            if m == total_no_spins - 2: 
-                m_matrix_list.append([0 for i in range(m-2)] + [np.e**(-m-2)] + [0] + [-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)] 
+        if m == 1:
+            local_list = [0] + [-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)] + [0] + [np.e**(-m+2)]; local_length = len(local_list)
+            m_matrix_list.append(local_list + [0 for k in range(M-local_length)])
+        if (m > 1) and (m < M - 2):  
+            local_list = [0 for j in range(m-2)]; local_length = len(local_list)
+            local_list += [np.e**(-m-2)] + [0] + [-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)] + [0] + [np.e**(-m+2)]
+            local_list += [0 for j in range(M-local_length - 5)]
+            m_matrix_list.append(local_list)
+        if m == total_no_spins - 2: 
+            m_matrix_list.append([0 for i in range(m-2)] + [np.e**(-m-2)] + [0] + [-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)] 
                                      + [0])
-            if m == total_no_spins - 1:
-                m_matrix_list.append([0 for i in range(m-2)] + [np.e**(-m-2)] + [0] + [-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)])
+        if m == total_no_spins - 1:
+            m_matrix_list.append([0 for i in range(m-2)] + [np.e**(-m-2)] + [0] + [-np.e**(-m) - np.e**(-m) + p ** np.e**(-0)])
             
-    else:
-        pass 
-    
     return (m_matrix_list)
 
 def M_matrix_test(total_no_spins, M_tensor):
